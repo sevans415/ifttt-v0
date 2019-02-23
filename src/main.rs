8d591b6926ed91324fcea65c99f539dc2c831229
Spencer Evans <spencer@commure.com>
2019-02-22 19:59:26 -0800

    rocket and cockroach set up with connection pooling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,76 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+// extern crate postgres;
+
+use rocket_contrib::databases::postgres;
+use postgres::{Connection, TlsMode, Result};
+use postgres::transaction::Transaction;
+use postgres::error::T_R_SERIALIZATION_FAILURE;
+
+#[database("postgres_logs")]
+struct LogsDbConn(postgres::Connection);
+
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index(conn: LogsDbConn) -> &'static str {
+  execute_txn(&conn, |txn| transfer_funds(txn, 1, 2, 100)).unwrap();
+  "Hello, world!"
+}
+
+/// Runs op inside a transaction and retries it as needed.
+/// On non-retryable failures, the transaction is aborted and
+/// rolled back; on success, the transaction is committed.
+fn execute_txn<T, F>(conn: &Connection, mut op: F) -> Result<T>
+where
+    F: Fn(&Transaction) -> Result<T>,
+{
+    let txn = conn.transaction()?;
+    loop {
+        let sp = txn.savepoint("cockroach_restart")?;
+        match op(&sp).and_then(|t| sp.commit().map(|_| t)) {
+            Err(ref err) if err.as_db()
+                               .map(|e| e.code == T_R_SERIALIZATION_FAILURE)
+                               .unwrap_or(false) => {},
+            r => break r,
+        }
+    }.and_then(|t| txn.commit().map(|_| t))
+}
+
+fn transfer_funds(txn: &Transaction, from: i64, to: i64, amount: i64) -> Result<()> {
+    // Read the balance.
+    let from_balance: i64 = txn.query("SELECT balance FROM accounts WHERE id = $1", &[&from])?
+        .get(0)
+        .get(0);
+
+    assert!(from_balance >= amount);
+
+    // Perform the transfer.
+    txn.execute(
+        "UPDATE accounts SET balance = balance - $1 WHERE id = $2",
+        &[&amount, &from],
+    )?;
+    txn.execute(
+        "UPDATE accounts SET balance = balance + $1 WHERE id = $2",
+        &[&amount, &to],
+    )?;
+    Ok(())
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    let conn = Connection::connect("postgresql://maxroach@localhost:26257/bank", TlsMode::None)
+        .unwrap();
+
+    // Run a transfer in a transaction.
+    // execute_txn(&conn, |txn| transfer_funds(txn, 1, 2, 100)).unwrap();
+
+    // Check account balances after the transaction.
+    for row in &conn.query("SELECT id, balance FROM accounts", &[]).unwrap() {
+        let id: i64 = row.get(0);
+        let balance: i64 = row.get(1);
+        println!("{} {}", id, balance);
+    }
+
+    rocket::ignite().attach(LogsDbConn::fairing()).mount("/", routes![index]).launch();
 }
